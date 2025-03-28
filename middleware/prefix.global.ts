d76bce7a7edeba5256f6afab334e3676f90bd530
youzq <yzq1024aswater@163.com>
2025-03-28 15:49:27 +0800

    调试

--- middleware/prefix.global.ts
+++ middleware/prefix.global.ts
@@ -4,16 +4,16 @@ export default defineNuxtRouteMiddleware((to, from) => {
 	// 仅在生产环境处理
 	if (process.env.NODE_ENV === 'production') {
 		const base = config.public.baseURL
-		console.log('生产环境处理', to, from)
+		console.log('生产环境处理', to.path)
 		// 排除base路径自身和已包含前缀的路径
-		if (!to.path.startsWith(base) && to.fullPath !== '/') {
-			return navigateTo({
-				path: `${base}${to.path.slice(1)}`,
-				query: to.query,
-				hash: to.hash
-			}, { redirectCode: 301 })
-		}
-		return navigateTo('/', { redirectCode: 301 }) // 重定向到新路径，保持查询参数和哈希
+		// if (!to.path.startsWith(base) ) {
+		// 	return navigateTo({
+		// 		path: `${base}${to.path.slice(1)}`,
+		// 		query: to.query,
+		// 		hash: to.hash
+		// 	}, { redirectCode: 301 })
+		// }
+
 
 	}
 })
